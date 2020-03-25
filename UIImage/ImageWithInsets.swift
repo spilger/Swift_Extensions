474e90b7ad656ac3f69b8f634bf5a473b91dd386
Michael <mispilger@gmail.com>
2020-03-25 14:35:07 +0100

    Added insets

---
+++ UIImage/ImageWithInsets.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage.swift
+//  Image Segmentor
+//
+//  Created by Michael Spilger on 29.02.20.
+//  Copyright © 2020 Michael Spilger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Accelerate
+
+extension UIImage {
+    func imageWithInsets(insets: UIEdgeInsets) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(
+            CGSize(width: self.size.width + insets.left + insets.right,
+                   height: self.size.height + insets.top + insets.bottom), false, self.scale)
+        let _ = UIGraphicsGetCurrentContext()
+        let origin = CGPoint(x: insets.left, y: insets.top)
+        self.draw(at: origin)
+        let imageWithInsets = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return imageWithInsets
+    }
+}
